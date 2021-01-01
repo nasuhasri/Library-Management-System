605b641b85c74fbb9f75e1ba111ea02669a0ceec
nasuhasri <nasuhasri00@gmail.com>
2021-01-01 23:36:01 +0800

    Add database

---
+++ Library Management System/BorrowersInfoPage.vb
@@ -0,0 +1,10 @@
+﻿Public Class BorrowersInfoPage
+    Private Sub Borrowers_Info_Page_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnReturn_Click(sender As Object, e As EventArgs) Handles btnReturn.Click
+        Me.Close()
+        MenuListPage.Show()
+    End Sub
+End Class
--- Library Management System/MenuListPage.vb
+++ Library Management System/MenuListPage.vb
@@ -1,3 +1,10 @@
 ﻿Public Class MenuListPage
+    Private Sub BorrowerDetailsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BorrowerDetailsToolStripMenuItem.Click
+        Me.Hide()
+        BorrowersInfoPage.Show()
+    End Sub
 
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
 End Class
