cee55055b8e2d14d11359b0b2fdc5495eeac18f8
nasuhasri <nasuhasri00@gmail.com>
2021-01-05 15:55:06 +0800

    update

--- Library Management System/My Project/Application.Designer.vb
+++ Library Management System/My Project/Application.Designer.vb
@@ -32,7 +32,11 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
+<<<<<<< Updated upstream
             Me.MainForm = Global.Library_Management_System.RegistrationPage
+=======
+            Me.MainForm = Global.Library_Management_System.MenuListPage
+>>>>>>> Stashed changes
         End Sub
     End Class
 End Namespace
